dd008e90edab018dd838d3e963cb80a7612d775b
Lukas Turcani <lukasturcani93@gmail.com>
2023-12-11 18:08:29 +0000

    Add task size

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -175,6 +175,28 @@ pub fn App(cx: Scope) -> Element {
                         }
                         div {
                             class: "mb-5",
+                            div {
+                                class: "inline-flex rounded-md shadow-sm",
+                                role: "group",
+                                button {
+                                    r#type: "button",
+                                    class: "px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-s-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700",
+                                    "Small",
+                                },
+                                button {
+                                    r#type: "button",
+                                    class: "px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border-t border-b border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700",
+                                    "Medium",
+                                },
+                                button {
+                                    r#type: "button",
+                                    class: "px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-e-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700",
+                                    "Large",
+                                }
+                            }
+                        },
+                        div {
+                            class: "mb-5",
                             label {
                                 r#for: "task_description" ,
                                 class: "block mb-2 text-sm font-medium text-gray-900 dark:text-white",
