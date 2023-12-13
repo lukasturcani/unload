a9aa63d0b2af54914320fc0ea95c338bd7373b64
Lukas Turcani <lukasturcani93@gmail.com>
2023-12-13 16:22:37 +0000

    reomve thing

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -206,7 +206,7 @@ pub fn App(cx: Scope) -> Element {
                             textarea {
                                 r#id: "task_description",
                                 rows: "4",
-                                class: "block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none",
+                                class: "block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
                                 placeholder: "Give a description...",
                             },
                         },
