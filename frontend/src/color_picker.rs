1f11317e0465ba93bcb452b54160d1f6711cfe62
Lukas Turcani <lukasturcani93@gmail.com>
2023-12-26 12:59:08 +0100

    add color selection

--- frontend/src/color_picker.rs
+++ frontend/src/color_picker.rs
@@ -22,28 +22,55 @@ const COLORS: [(Color, &str, &str); 16] = [
 
 #[component]
 pub fn ColorPicker(cx: Scope) -> Element {
+    let selected = use_state(cx, || Color::Black);
     cx.render(rsx! {
         div {
             class: "flex-1 flex grid grid-cols-4 gap-4 justify-items-center",
             for (color, name, class) in COLORS.iter() {rsx! {
-                div {
-                    class: "group relative",
+                if color == &**selected {rsx!{
                     div {
-                        class: "w-8 h-8 rounded cursor-pointer {class}",
-                    },
-                    div {
-                        class: "
-                            pointer-events-none absolute -top-10 left-0 w-max
-                            opacity-0 transition-opacity group-hover:opacity-100
-                            z-10 inline-block px-3 py-2 text-sm font-medium text-white
-                            bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-800",
-                        "{name}"
+                        class: "group relative",
                         div {
-                            class: "tooltip-arrow",
-                            "data-popper-arrow": "",
+                            class: "
+                                w-8 h-8 rounded cursor-pointer {class}
+                                ring-blue-500 dark:ring-blue-600 ring-2",
+                            onclick: |_| selected.set(*color),
+                        },
+                        div {
+                            class: "
+                                pointer-events-none absolute -top-10 left-0 w-max
+                                opacity-0 transition-opacity group-hover:opacity-100
+                                z-10 inline-block px-3 py-2 text-sm font-medium text-white
+                                bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-800",
+                            "{name}"
+                            div {
+                                class: "tooltip-arrow",
+                                "data-popper-arrow": "",
+                            }
                         }
                     }
-                }
+                }} else {rsx!{
+                    div {
+                        class: "group relative",
+                        div {
+                            class: "w-8 h-8 rounded cursor-pointer {class}",
+                            onclick: |_| selected.set(*color),
+                        },
+                        div {
+                            class: "
+                                pointer-events-none absolute -top-10 left-0 w-max
+                                opacity-0 transition-opacity group-hover:opacity-100
+                                z-10 inline-block px-3 py-2 text-sm font-medium text-white
+                                bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-800",
+                            "{name}"
+                            div {
+                                class: "tooltip-arrow",
+                                "data-popper-arrow": "",
+                            }
+                        }
+                    }
+
+                }}
             }}
         }
     })
