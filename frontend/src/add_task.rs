4813b681e96b774b2fa54007522b54b9b59d2ed0
Lukas Turcani <lukasturcani93@gmail.com>
2023-12-26 16:28:29 +0100

    fix

--- frontend/src/add_task.rs
+++ frontend/src/add_task.rs
@@ -530,6 +530,7 @@ fn UserSearch<'a>(
                 id: *id,
                 class: "block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
                 placeholder: "Search",
+                autocomplete: "off",
                 value: "{search_input}",
                 onfocusin: |_| has_input_focus.set(true),
                 onfocusout: |_| has_input_focus.set(false),
