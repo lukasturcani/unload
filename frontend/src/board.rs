f1c9a23fb920563fcaa318969a3b01d844623964
Lukas Turcani <lukasturcani93@gmail.com>
2023-12-26 01:53:54 +0100

    add hover

--- frontend/src/board.rs
+++ frontend/src/board.rs
@@ -151,7 +151,9 @@ fn Task(cx: Scope, task_id: TaskId) -> Element {
     cx.render(rsx! {
         div {
             draggable: true,
-            class: "block w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-700 dark:border-gray-700 dark:hover:bg-gray-700",
+            class: "
+                block w-full p-6 bg-white border border-gray-200 rounded-lg shadow
+                hover:bg-gray-100 dark:bg-gray-700 dark:border-gray-700 dark:hover:bg-gray-600",
             h5 {
                 class: "mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white",
                 "{data.title}",
