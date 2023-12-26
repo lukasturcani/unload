7ab12718553620c45144027890d5e015fb5eb750
Lukas Turcani <lukasturcani93@gmail.com>
2023-12-26 20:04:52 +0100

    improve padding

--- frontend/src/board.rs
+++ frontend/src/board.rs
@@ -158,7 +158,7 @@ fn Task(cx: Scope, task_id: TaskId) -> Element {
         div {
             draggable: true,
             class: "
-                block w-full p-6 bg-white border border-gray-200 rounded-lg shadow
+                block w-full p-3 bg-white border border-gray-200 rounded-lg shadow
                 hover:bg-gray-100 dark:bg-gray-700 dark:border-gray-700 dark:hover:bg-gray-600",
             h5 {
                 class: "mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white",
