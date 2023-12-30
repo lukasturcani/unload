71cc03c96a36bdb07c85c3d46f838ff765573ee2
Lukas Turcani <lukasturcani93@gmail.com>
2023-12-30 14:06:43 +0100

    stuff

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -646,24 +646,33 @@ WHERE
 pub async fn update_task_assignees(
     State(pool): State<SqlitePool>,
     Path((board_name, task_id)): Path<(BoardName, TaskId)>,
-    Json(due): Json<Vec<UserId>>,
+    Json(assignees): Json<Vec<UserId>>,
 ) -> Result<Json<()>> {
     let mut tx = pool.begin().await?;
     sqlx::query!(
         "
-UPDATE
+DELETE FROM
     task_assignments
-SET
-    due = ?
 WHERE
-   board_name = ? AND id = ?
+   board_name = ? AND task_id = ?
 ",
-        due,
         board_name,
         task_id
     )
     .execute(&mut *tx)
     .await?;
+    for user_id in assignees {
+        sqlx::query!(
+            "
+INSERT INTO task_assignments (board_name, user_id, task_id)
+VALUES (?, ?, ?)",
+            board_name,
+            user_id,
+            task_id,
+        )
+        .execute(&mut *tx)
+        .await?;
+    }
     tx.commit().await?;
     Ok(Json(()))
 }
