ffa97f7831aa81d5a15e97fbc90ab575ac12e58d
Lukas Turcani <lukasturcani93@gmail.com>
2023-12-30 18:51:43 +0100

    stuff

--- frontend/src/board.rs
+++ frontend/src/board.rs
@@ -88,8 +88,18 @@ fn ToDoColumn(cx: Scope) -> Element {
             class: COLUMN,
             div {
                 class: "flex items-center gap-2",
-                span {
-                    class: "flex w-4 h-4 bg-red-500 rounded-full flex-shrink-0",
+                svg {
+                    xmlns: "http://www.w3.org/2000/svg",
+                    fill: "none",
+                    "viewBox": "0 0 24 24",
+                    "stroke-width": "1.5",
+                    stroke: "white",
+                    class: "w-8 h-8",
+                    path {
+                        "stroke-linecap": "round",
+                        "stroke-linejoin": "round",
+                        d: "M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z",
+                    }
                 }
                 h2 {
                     class: COLUMN_HEADING,
@@ -118,9 +128,21 @@ fn InProgressColumn(cx: Scope) -> Element {
             class: COLUMN,
             div {
                 class: "flex items-center gap-2",
-                span {
-                    class: "flex w-4 h-4 bg-yellow-300 rounded-full flex-shrink-0",
+                svg {
+                    xmlns: "http://www.w3.org/2000/svg",
+                    fill: "none",
+                    "viewBox": "0 0 24 24",
+                    "stroke-width": "1.5",
+                    "stroke": "white",
+                    "class": "w-8 h-8",
+                    path {
+                        "stroke-linecap": "round",
+                        "stroke-linejoin": "round",
+                        d: "M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z",
+                    }
                 }
+
+
                 h2 {
                     class: COLUMN_HEADING,
                     "In Progress"
@@ -148,8 +170,18 @@ fn DoneColumn(cx: Scope) -> Element {
             class: COLUMN,
             div {
                 class: "flex items-center gap-2",
-                span {
-                    class: "flex w-4 h-4 bg-green-500 rounded-full flex-shrink-0",
+                svg {
+                    xmlns: "http://www.w3.org/2000/svg",
+                    fill: "none",
+                    "viewBox": "0 0 24 24",
+                    "stroke-width": "1.5",
+                    stroke: "white",
+                    class: "w-8 h-8",
+                    path {
+                        "stroke-linecap": "round",
+                        "stroke-linejoin": "round",
+                        d: "M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z",
+                    }
                 }
                 h2 {
                     class: COLUMN_HEADING,
