46150ec1f4188f8b4760cfbdb6e9fbb112ad2ddd
Lukas Turcani <lukasturcani93@gmail.com>
2023-12-30 22:18:57 +0000

    add icons

--- frontend/src/board.rs
+++ frontend/src/board.rs
@@ -513,7 +513,10 @@ fn Due(cx: Scope, task_id: TaskId, due: Option<DueOptions>) -> Element {
                 }
                 button {
                     r#type: "button",
-                    onclick: |_| {
+                    class: "rounded-lg p-1.5 inline-flex items-center justify-center h-8 w-8 bg-inherit",
+                    prevent_default: "onclick",
+                    onclick: |event| {
+                        event.stop_propagation();
                         editing.set(false);
                         set_task_due(
                             model.clone(),
@@ -525,12 +528,41 @@ fn Due(cx: Scope, task_id: TaskId, due: Option<DueOptions>) -> Element {
                             })
                         )
                     },
-                    "V"
+                    svg {
+                        xmlns: "http://www.w3.org/2000/svg",
+                        fill: "none",
+                        "viewBox": "0 0 24 24",
+                        "stroke-width": "1.5",
+                        stroke: "white",
+                        class: "w-6 h-6",
+                        path {
+                            "stroke-linecap": "round",
+                            "stroke-linejoin": "round",
+                            d: "m4.5 12.75 6 6 9-13.5",
+                        }
+                    }
                 }
                 button {
                     r#type: "button",
-                    onclick: |_| editing.set(false),
-                    "X"
+                    class: "rounded-lg p-1.5 inline-flex items-center justify-center h-8 w-8 bg-inherit",
+                    prevent_default: "onclick",
+                    onclick: |event| {
+                        event.stop_propagation();
+                        editing.set(false);
+                    },
+                    svg {
+                        xmlns: "http://www.w3.org/2000/svg",
+                        fill: "none",
+                        "viewBox": "0 0 24 24",
+                        "stroke-width": "1.5",
+                        stroke: "white",
+                        class: "w-6 h-6",
+                        path {
+                            "stroke-linecap": "round",
+                            "stroke-linejoin": "round",
+                            d: "M6 18 18 6M6 6l12 12",
+                        }
+                    }
                 }
             }
         }} else {rsx!{
