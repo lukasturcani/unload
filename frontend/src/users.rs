d6509243b18d46099a730d620aa367c03b379389
Lukas Turcani <lukasturcani93@gmail.com>
2024-01-06 15:48:44 +0000

    stuff

--- frontend/src/users.rs
+++ frontend/src/users.rs
@@ -2,6 +2,9 @@ use crate::{model::Model, requests};
 use dioxus::prelude::*;
 use shared_models::BoardName;
 use crate::color_picker;
+use shared_models::Color;
+
+enum Column { Color, Name }
 
 #[component]
 pub fn Users(cx: Scope, board_name: BoardName) -> Element {
@@ -11,6 +14,9 @@ pub fn Users(cx: Scope, board_name: BoardName) -> Element {
     }
     use_future(cx, (), |_| requests::board(model.clone()));
     let users = &model.read().users;
+    let edit_field = use_state(cx, || None::<(usize, Column)>);
+    let color = use_state(cx, || Color::Black);
+    let name = use_state(cx, String::new);
     cx.render(rsx! {
         div {
             class: "w-full p-2",
@@ -34,21 +40,37 @@ pub fn Users(cx: Scope, board_name: BoardName) -> Element {
                         }
                     }
                     tbody {
-                        for (i, (user_id, user)) in users.iter().enumerate() {
+                        for (row_index, (user_id, user)) in users.iter().enumerate() {
                             tr {
-                                class: if i == users.len() - 1 {
+                                class: if row_index == users.len() - 1 {
                                     "bg-gray-800 sm:hover:bg-gray-600 border-gray-700"
                                 } else {
                                     "bg-gray-800 sm:hover:bg-gray-600 border-gray-700 border-b"
                                 },
                                 td {
                                     class: "px-6 py-4",
+                                    onclick: {
+                                        let model = model.clone();
+                                        let user_id = user_id.clone();
+                                        move |_| {
+                                            color.set(model.read().users[&user_id].color);
+                                            edit_field.set(Some((row_index, Column::Color)));
+                                        }
+                                    },
                                     div {
                                         class: "w-8 h-8 rounded cursor-pointer bg-blue-500 {color_picker::class(&user.color)}",
                                     },
                                 }
                                 td {
                                     class: "px-6 py-4",
+                                    onclick: {
+                                            let model = model.clone();
+                                            let user_id = user_id.clone();
+                                            move |_| {
+                                            name.set(model.read().users[&user_id].name.clone());
+                                            edit_field.set(Some((row_index, Column::Name)));
+                                        }
+                                    },
                                     "{user.name}"
                                 }
                             }
