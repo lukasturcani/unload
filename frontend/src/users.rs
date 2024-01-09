01eb1bbcbbdff8e8a8f691a158092ecc327eac56
Lukas Turcani <lukasturcani93@gmail.com>
2024-01-09 19:46:33 +0000

    add deleting users

--- frontend/src/users.rs
+++ frontend/src/users.rs
@@ -41,6 +41,10 @@ pub fn Users(cx: Scope, board_name: BoardName) -> Element {
                                 class: "px-6 py-3",
                                 "User"
                             }
+                            th {
+                                scope: "col",
+                                class: "px-6 py-3",
+                            }
                         }
                     }
                     tbody {
@@ -85,6 +89,13 @@ pub fn Users(cx: Scope, board_name: BoardName) -> Element {
                                                     "stroke-width": "1.5",
                                                     stroke: "currentColor",
                                                     class: "w-4 h-4",
+                                                    onclick: {
+                                                        let user_id = user_id.clone();
+                                                        move |_| {
+                                                            color.set(model.read().users[&user_id].color);
+                                                            edit_field.set(Some((row_index, Column::Color)));
+                                                        }
+                                                    },
                                                     path {
                                                         "stroke-linecap": "round",
                                                         "stroke-linejoin": "round",
@@ -133,6 +144,13 @@ pub fn Users(cx: Scope, board_name: BoardName) -> Element {
                                                     "stroke-width": "1.5",
                                                     stroke: "currentColor",
                                                     class: "w-4 h-4",
+                                                    onclick: {
+                                                        let user_id = user_id.clone();
+                                                        move |_| {
+                                                            name.set(model.read().users[&user_id].name.clone());
+                                                            edit_field.set(Some((row_index, Column::Name)));
+                                                        }
+                                                    },
                                                     path {
                                                         "stroke-linecap": "round",
                                                         "stroke-linejoin": "round",
@@ -141,6 +159,31 @@ pub fn Users(cx: Scope, board_name: BoardName) -> Element {
                                                 }
                                             }
                                         },
+                                    }
+                                }
+                                td {
+                                    class: "px-6 py-4",
+                                    div {
+                                        class: "grid grid-rows-1 place-items-end text-red-600",
+                                        svg {
+                                            xmlns: "http://www.w3.org/2000/svg",
+                                            fill: "none",
+                                            "viewBox": "0 0 24 24",
+                                            "stroke-width": "1.5",
+                                            stroke: "currentColor",
+                                            class: "w-6 h-6 cursor-pointer",
+                                            onclick: {
+                                                let user_id = *user_id;
+                                                move |_| {
+                                                    delete_user(model.clone(), user_id)
+                                                }
+                                            },
+                                            path {
+                                                "stroke-linecap": "round",
+                                                "stroke-linejoin": "round",
+                                                d: "m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0",
+                                            }
+                                        }
                                     }
                                 }
                             }
@@ -206,6 +249,34 @@ async fn send_set_user_name_request(
     Ok(reqwest::Client::new()
         .put(url)
         .json(&name)
+        .send()
+        .await?
+        .json::<()>()
+        .await?)
+}
+
+async fn delete_user(model: UseSharedState<Model>, user_id: UserId) {
+    if send_delete_user_request(model.clone(), user_id)
+        .await
+        .is_ok()
+    {
+        requests::board(model).await;
+    }
+}
+
+async fn send_delete_user_request(
+    model: UseSharedState<Model>,
+    user_id: UserId,
+) -> Result<(), anyhow::Error> {
+    let url = {
+        let model = model.read();
+        model.url.join(&format!(
+            "/api/boards/{}/users/{}",
+            model.board_name, user_id
+        ))?
+    };
+    Ok(reqwest::Client::new()
+        .delete(url)
         .send()
         .await?
         .json::<()>()
