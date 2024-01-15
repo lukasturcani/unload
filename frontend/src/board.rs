1252a20b2c7000b212d9f0af09bae992a46b95ae
Lukas Turcani <lukasturcani93@gmail.com>
2024-01-15 12:35:21 +0000

    fix stuff

--- frontend/src/board.rs
+++ frontend/src/board.rs
@@ -124,28 +124,6 @@ fn OneColumnBoard(cx: Scope, board_name: BoardName) -> Element {
                         }}
                     }
                     button {
-                        r#type: "button" ,
-                        class: "inline-flex flex-col items-center justify-center px-5 border-e active:bg-gray-800 group border-gray-600",
-                        onclick: |_| {
-                            nav.push(Route::AddUser {
-                                board_name: board_name.clone(),
-                            });
-                        },
-                        svg {
-                            xmlns: "http://www.w3.org/2000/svg",
-                            fill: "none",
-                            "viewBox": "0 0 24 24",
-                            "stroke-width": "1.5",
-                            stroke: "currentColor",
-                            class: "mb-2 w-6 h-6 text-gray-400 group-active:text-blue-500",
-                            path {
-                                "stroke-linecap": "round",
-                                "stroke-linejoin": "round",
-                                d: "M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z",
-                            }
-                        }
-                    }
-                    button {
                         r#type: "button",
                         class: "inline-flex flex-col items-center justify-center px-5 border-e active:bg-gray-800 group border-gray-600",
                         onclick: |_| {
@@ -164,6 +142,28 @@ fn OneColumnBoard(cx: Scope, board_name: BoardName) -> Element {
                                 "stroke-linecap": "round",
                                 "stroke-linejoin": "round",
                                 d: "M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z",
+                            }
+                        }
+                    }
+                    button {
+                        r#type: "button" ,
+                        class: "inline-flex flex-col items-center justify-center px-5 border-e active:bg-gray-800 group border-gray-600",
+                        onclick: |_| {
+                            nav.push(Route::AddUser {
+                                board_name: board_name.clone(),
+                            });
+                        },
+                        svg {
+                            xmlns: "http://www.w3.org/2000/svg",
+                            fill: "none",
+                            "viewBox": "0 0 24 24",
+                            "stroke-width": "1.5",
+                            stroke: "currentColor",
+                            class: "mb-2 w-6 h-6 text-gray-400 group-active:text-blue-500",
+                            path {
+                                "stroke-linecap": "round",
+                                "stroke-linejoin": "round",
+                                d: "M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z",
                             }
                         }
                     }
