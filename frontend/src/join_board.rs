210cfbd646af14baa21b65c38ce2f01feacdcaca
Lukas Turcani <lukasturcani93@gmail.com>
2024-01-16 09:56:38 +0000

    remove dark mode

--- frontend/src/join_board.rs
+++ frontend/src/join_board.rs
@@ -8,10 +8,10 @@ use reqwest::Client;
 use shared_models::BoardName;
 
 const TEXT_INPUT: &str = "
-    bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-    focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700
-    dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
-    dark:focus:border-blue-500
+    border text-sm rounded-lg
+    block p-2.5 bg-gray-700
+    border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500
+    focus:border-blue-500
 ";
 
 #[component]
@@ -50,10 +50,10 @@ pub fn JoinBoard(cx: Scope) -> Element {
             div {
                 class: "inline-flex items-center justify-center w-full",
                 hr {
-                    class: "w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700",
+                    class: "w-64 h-px my-8 border-0 bg-gray-700",
                 },
                 span {
-                    class: "absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900",
+                    class: "absolute px-3 font-medium -translate-x-1/2 left-1/2 text-white bg-gray-900",
                     "or"
                 },
             },
