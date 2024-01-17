222d5d30d57719da0a6f252369aae0a5b9c5b1f9
Lukas Turcani <lukasturcani93@gmail.com>
2024-01-17 12:02:02 +0000

    wip

--- frontend/src/board.rs
+++ frontend/src/board.rs
@@ -574,7 +574,7 @@ fn Task(cx: Scope, task_id: TaskId, status: TaskStatus) -> Element {
                 }
             }
             div {
-                class: "flex flex-row gap-1",
+                class: "flex flex-row gap-2 flex-wrap",
                 if **editing_size {rsx!{
                     span {
                         class: "text-sm font-medium px-2.5 py-0.5 rounded bg-green-900 text-green-300 cursor-pointer",
@@ -1076,134 +1076,131 @@ fn Tags(cx: Scope, task_id: TaskId) -> Element {
     let assigned_tags = use_ref(cx, Vec::new);
     let clicked_tags = use_ref(cx, HashSet::new);
     cx.render(rsx! {
+        for (tag_id, tag) in tags {rsx!{
+            span {
+                class: "
+                    text-sm font-medium px-2.5 py-0.5 rounded
+                    {tag_bg(&clicked_tags, tag_id, &tag.color)}
+                    text-white cursor-pointer
+                    border {color_picker::border_class(&tag.color)}
+                ",
+                onclick: {
+                    let tag_id = *tag_id;
+                    move |event| {
+                        event.stop_propagation();
+                        let mut clicked_tags = clicked_tags.write();
+                        if clicked_tags.contains(&tag_id) {
+                            clicked_tags.remove(&tag_id);
+                        } else {
+                            clicked_tags.insert(tag_id);
+                        }
+                    }
+                },
+                "# {tag.name}",
+            }
+        }}
         div {
-            class: "flex flex-row gap-2",
-            for (tag_id, tag) in tags {rsx!{
-                span {
+            class: "group relative",
+            svg {
+                xmlns: "http://www.w3.org/2000/svg",
+                fill: "none",
+                "viewBox": "0 0 24 24" ,
+                "stroke-width": "1.5" ,
+                stroke: "white" ,
+                class: "w-6 h-6 border border-white rounded cursor-pointer",
+                prevent_default: "onclick",
+                onclick: |event| {
+                    event.stop_propagation();
+                    *assigned_tags.write() = model.read().tasks[task_id].tags.clone();
+                    show_assign_tag.set(true);
+                },
+                path {
+                    "stroke-linecap": "round",
+                    "stroke-linejoin": "round",
+                    d: "M12 4.5v15m7.5-7.5h-15",
+                }
+            }
+            if **show_assign_tag {rsx!{
+                div {
                     class: "
-                        text-sm font-medium px-2.5 py-0.5 rounded
-                        {tag_bg(&clicked_tags, tag_id, &tag.color)}
-                        text-white cursor-pointer
-                        border {color_picker::border_class(&tag.color)}
-                    ",
-                    onclick: {
-                        let tag_id = *tag_id;
-                        move |event| {
-                            event.stop_propagation();
-                            let mut clicked_tags = clicked_tags.write();
-                            if clicked_tags.contains(&tag_id) {
-                                clicked_tags.remove(&tag_id);
-                            } else {
-                                clicked_tags.insert(tag_id);
+                        flex flex-col gap-2
+                        absolute -top-10 -left-2 w-72
+                        z-10 px-3 py-2 text-sm font-medium text-white
+                        rounded-lg shadow-sm bg-gray-800
+                        border border-gray-700",
+                    TagSearch {
+                        id: "assign_tag_modal",
+                        on_select_tag: |tag_id| assigned_tags.write().push(tag_id),
+                        on_remove_tag: |tag_id| assigned_tags.write().retain(|&value| value != tag_id),
+                        initial_tags: assigned_tags.read().clone(),
+                        always_show_suggestions: true,
+                    }
+                    div {
+                        class: "flex flex-row gap-2 justify-end",
+                        button {
+                            r#type: "button",
+                            class: "
+                                rounded-lg p-1.5 inline-flex items-center justify-center h-8 w-8
+                                border border-green-500 text-green-500 hover:bg-green-500 hover:text-white",
+                            prevent_default: "onclick",
+                            onclick: |event| {
+                                event.stop_propagation();
+                                show_assign_tag.set(false);
+                                set_task_tags(
+                                    model.clone(),
+                                    *task_id, assigned_tags.read().clone()
+                                )
+                            },
+                            svg {
+                                xmlns: "http://www.w3.org/2000/svg",
+                                fill: "none",
+                                "viewBox": "0 0 24 24",
+                                "stroke-width": "1.5",
+                                stroke: "currentColor",
+                                class: "w-6 h-6",
+                                path {
+                                    "stroke-linecap": "round",
+                                    "stroke-linejoin": "round",
+                                    d: "m4.5 12.75 6 6 9-13.5",
+                                }
                             }
                         }
-                    },
-                    "# {tag.name}",
+                        button {
+                            r#type: "button",
+                            prevent_default: "onclick",
+                            class: "
+                                rounded-lg p-1.5 inline-flex items-center justify-center h-8 w-8
+                                border border-red-500 text-red-500 hover:bg-red-500 hover:text-white",
+                            onclick: |event| {
+                                event.stop_propagation();
+                                show_assign_tag.set(false);
+                            },
+                            svg {
+                                xmlns: "http://www.w3.org/2000/svg",
+                                fill: "none",
+                                "viewBox": "0 0 24 24",
+                                "stroke-width": "1.5",
+                                stroke: "currentColor",
+                                class: "w-6 h-6",
+                                path {
+                                    "stroke-linecap": "round",
+                                    "stroke-linejoin": "round",
+                                    d: "M6 18 18 6M6 6l12 12",
+                                }
+                            }
+                        }
+                    }
+                }
+            }} else {rsx!{
+                div {
+                    class: TOOLTIP,
+                    "Assign Tag"
+                    div {
+                        class: "tooltip-arrow",
+                        "data-popper-arrow": "",
+                    }
                 }
             }}
-            div {
-                class: "group relative",
-                svg {
-                    xmlns: "http://www.w3.org/2000/svg",
-                    fill: "none",
-                    "viewBox": "0 0 24 24" ,
-                    "stroke-width": "1.5" ,
-                    stroke: "white" ,
-                    class: "w-6 h-6 border border-white rounded cursor-pointer",
-                    prevent_default: "onclick",
-                    onclick: |event| {
-                        event.stop_propagation();
-                        *assigned_tags.write() = model.read().tasks[task_id].tags.clone();
-                        show_assign_tag.set(true);
-                    },
-                    path {
-                        "stroke-linecap": "round",
-                        "stroke-linejoin": "round",
-                        d: "M12 4.5v15m7.5-7.5h-15",
-                    }
-                }
-                if **show_assign_tag {rsx!{
-                    div {
-                        class: "
-                            flex flex-col gap-2
-                            absolute -top-10 -left-2 w-72
-                            z-10 px-3 py-2 text-sm font-medium text-white
-                            rounded-lg shadow-sm bg-gray-800
-                            border border-gray-700",
-                        TagSearch {
-                            id: "assign_tag_modal",
-                            on_select_tag: |tag_id| assigned_tags.write().push(tag_id),
-                            on_remove_tag: |tag_id| assigned_tags.write().retain(|&value| value != tag_id),
-                            initial_tags: assigned_tags.read().clone(),
-                            always_show_suggestions: true,
-                        }
-                        div {
-                            class: "flex flex-row gap-2 justify-end",
-                            button {
-                                r#type: "button",
-                                class: "
-                                    rounded-lg p-1.5 inline-flex items-center justify-center h-8 w-8
-                                    border border-green-500 text-green-500 hover:bg-green-500 hover:text-white",
-                                prevent_default: "onclick",
-                                onclick: |event| {
-                                    event.stop_propagation();
-                                    show_assign_tag.set(false);
-                                    set_task_tags(
-                                        model.clone(),
-                                        *task_id, assigned_tags.read().clone()
-                                    )
-                                },
-                                svg {
-                                    xmlns: "http://www.w3.org/2000/svg",
-                                    fill: "none",
-                                    "viewBox": "0 0 24 24",
-                                    "stroke-width": "1.5",
-                                    stroke: "currentColor",
-                                    class: "w-6 h-6",
-                                    path {
-                                        "stroke-linecap": "round",
-                                        "stroke-linejoin": "round",
-                                        d: "m4.5 12.75 6 6 9-13.5",
-                                    }
-                                }
-                            }
-                            button {
-                                r#type: "button",
-                                prevent_default: "onclick",
-                                class: "
-                                    rounded-lg p-1.5 inline-flex items-center justify-center h-8 w-8
-                                    border border-red-500 text-red-500 hover:bg-red-500 hover:text-white",
-                                onclick: |event| {
-                                    event.stop_propagation();
-                                    show_assign_tag.set(false);
-                                },
-                                svg {
-                                    xmlns: "http://www.w3.org/2000/svg",
-                                    fill: "none",
-                                    "viewBox": "0 0 24 24",
-                                    "stroke-width": "1.5",
-                                    stroke: "currentColor",
-                                    class: "w-6 h-6",
-                                    path {
-                                        "stroke-linecap": "round",
-                                        "stroke-linejoin": "round",
-                                        d: "M6 18 18 6M6 6l12 12",
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }} else {rsx!{
-                    div {
-                        class: TOOLTIP,
-                        "Assign Tag"
-                        div {
-                            class: "tooltip-arrow",
-                            "data-popper-arrow": "",
-                        }
-                    }
-                }}
-            }
         }
     })
 }
