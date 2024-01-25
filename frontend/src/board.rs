4223af7643493f4241752e16296c9dc566ba98aa
Lukas Turcani <lukasturcani93@gmail.com>
2024-01-25 17:28:39 +0000

    wip

--- frontend/src/board.rs
+++ frontend/src/board.rs
@@ -27,24 +27,14 @@ pub const DENSE_COLUMN_TASK_LIST: &str = "grow flex flex-col overflow-y-scroll";
 
 #[component]
 pub fn Board(cx: Scope, board_name: BoardName) -> Element {
-    let model = use_shared_state::<Model>(cx).unwrap();
     let layout = ResponsiveLayout::from_window();
     let eval = use_eval(cx);
     eval(&format!(r#"document.title = "{board_name}";"#)).unwrap();
     cx.render(rsx! {
         match layout {
             ResponsiveLayout::Narrow => rsx! {
-                match model.read().dense_view {
-                    true => rsx! {
-                        DenseOneColumnBoard {
-                            board_name: board_name.clone(),
-                        }
-                    },
-                    false => rsx! {
-                        OneColumnBoard {
-                            board_name: board_name.clone(),
-                        }
-                    }
+                OneColumnBoard {
+                    board_name: board_name.clone(),
                 }
             },
             ResponsiveLayout::Wide => rsx! {
@@ -71,10 +61,13 @@ fn OneColumnBoard(cx: Scope, board_name: BoardName) -> Element {
             class: "flex flex-col bg-gray-900 h-screen w-screen gap-1",
             div {
                 class: "grow grid grid-cols-1 p-1 overflow-y-auto",
-                match **column {
-                    TaskStatus::ToDo => rsx! { ToDoColumn {} },
-                    TaskStatus::InProgress => rsx! { InProgressColumn {} },
-                    TaskStatus::Done => rsx! { DoneColumn {} },
+                match (**column, model.read().dense_view) {
+                    (TaskStatus::ToDo, false) => rsx! { ToDoColumn {} },
+                    (TaskStatus::InProgress, false) => rsx! { InProgressColumn {} },
+                    (TaskStatus::Done, false) => rsx! { DoneColumn {} },
+                    (TaskStatus::ToDo, true) => rsx! { DenseToDoColumn {} },
+                    (TaskStatus::InProgress, true) => rsx! { DenseInProgressColumn {} },
+                    (TaskStatus::Done, true) => rsx! { DenseDoneColumn {} },
                 }
             }
             div {
@@ -246,195 +239,6 @@ fn OneColumnBoard(cx: Scope, board_name: BoardName) -> Element {
     })
 }
 
-#[component]
-fn DenseOneColumnBoard(cx: Scope, board_name: BoardName) -> Element {
-    let model = use_shared_state::<Model>(cx).unwrap();
-    if &model.read().board_name != board_name {
-        model.write().board_name = board_name.clone()
-    }
-    let nav = use_navigator(cx);
-    let column = use_state(cx, || TaskStatus::ToDo);
-    let show_filters = use_state(cx, || false);
-    use_future(cx, (), |_| requests::board(model.clone()));
-    cx.render(rsx! {
-        div {
-            class: "flex flex-col bg-gray-900 h-screen w-screen gap-1",
-            div {
-                class: "grow grid grid-cols-1 p-1 overflow-y-auto",
-                match **column {
-                    TaskStatus::ToDo => rsx! { DenseToDoColumn {} },
-                    TaskStatus::InProgress => rsx! { DenseInProgressColumn {} },
-                    TaskStatus::Done => rsx! { DenseDoneColumn {} },
-                }
-            }
-            div {
-                class: "flex flex-col",
-                if **show_filters {rsx!{
-                   div {
-                       class: "w-full bg-gray-800 flex flex-col gap-2 p-2",
-                        TagFilter {}
-                        UserFilter {}
-                        SizeFilter {}
-                   }
-                }}
-                div {
-                    class: styles::BOTTOM_BAR,
-                    button {
-                        r#type: "button" ,
-                        class: styles::BOTTOM_BAR_BUTTON,
-                        disabled: **column == TaskStatus::ToDo,
-                        onclick: |_| {
-                            match **column {
-                                TaskStatus::ToDo => column.set(TaskStatus::ToDo),
-                                TaskStatus::InProgress => column.set(TaskStatus::ToDo),
-                                TaskStatus::Done => column.set(TaskStatus::InProgress),
-                            }
-                        },
-                        if **column != TaskStatus::ToDo {rsx!{
-                            svg {
-                                xmlns: "http://www.w3.org/2000/svg",
-                                fill: "none",
-                                "viewBox": "0 0 24 24",
-                                "stroke-width": "1.5",
-                                stroke: "currentColor",
-                                class: "w-6 h-6 text-gray-400 group-active:text-blue-500",
-                                path {
-                                    "stroke-linecap": "round",
-                                    "stroke-linejoin": "round",
-                                    d: "M15.75 19.5 8.25 12l7.5-7.5",
-                                }
-                            }
-                        }}
-                    }
-                    button {
-                        r#type: "button" ,
-                        class: if **show_filters {
-                            styles::BOTTOM_BAR_ACTIVE_BUTTON
-                        } else {
-                            styles::BOTTOM_BAR_BUTTON
-                        },
-                        onclick: |_| show_filters.set(!**show_filters),
-                        svg {
-                            xmlns: "http://www.w3.org/2000/svg",
-                            fill: "none",
-                            "viewBox": "0 0 24 24",
-                            "stroke-width": "1.5",
-                            stroke: "currentColor",
-                            class: if **show_filters {
-                                "w-6 h-6 text-blue-500"
-                            } else {
-                                "w-6 h-6 text-gray-400 group-active:text-blue-500"
-                            },
-                            path {
-                                "stroke-linecap": "round",
-                                "stroke-linejoin": "round",
-                                d: "M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z",
-                            }
-                        }
-                    }
-                    button {
-                        r#type: "button" ,
-                        class: styles::BOTTOM_BAR_BUTTON,
-                        onclick: |_| {
-                            nav.push(Route::Tags {
-                                board_name: board_name.clone(),
-                            });
-                        },
-                        svg {
-                            xmlns: "http://www.w3.org/2000/svg",
-                            fill: "none",
-                            "viewBox": "0 0 24 24",
-                            "stroke-width": "1.5",
-                            stroke: "currentColor",
-                            class: "w-6 h-6 text-gray-400 group-active:text-blue-500",
-                            path {
-                                "stroke-linecap": "round",
-                                "stroke-linejoin": "round",
-                                d: "M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z",
-                            }
-                            path {
-                                "stroke-linecap": "round",
-                                "stroke-linejoin": "round",
-                                d: "M6 6h.008v.008H6V6Z",
-                            }
-                        }
-                    }
-                    button {
-                        r#type: "button",
-                        class: styles::BOTTOM_BAR_BUTTON,
-                        onclick: |_| {
-                            nav.push(Route::Users {
-                                board_name: board_name.clone(),
-                            });
-                        },
-                        svg {
-                            xmlns: "http://www.w3.org/2000/svg",
-                            fill: "none",
-                            "viewBox": "0 0 24 24",
-                            "stroke-width": "1.5",
-                            stroke: "currentColor",
-                            class: "w-6 h-6 text-gray-400 group-active:text-blue-500",
-                            path {
-                                "stroke-linecap": "round",
-                                "stroke-linejoin": "round",
-                                d: "M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z",
-                            }
-                        }
-                    }
-                    button {
-                        r#type: "button" ,
-                        class: styles::BOTTOM_BAR_BUTTON,
-                        onclick: |_| {
-                            nav.push(Route::AddUser {
-                                board_name: board_name.clone(),
-                            });
-                        },
-                        svg {
-                            xmlns: "http://www.w3.org/2000/svg",
-                            fill: "none",
-                            "viewBox": "0 0 24 24",
-                            "stroke-width": "1.5",
-                            stroke: "currentColor",
-                            class: "w-6 h-6 text-gray-400 group-active:text-blue-500",
-                            path {
-                                "stroke-linecap": "round",
-                                "stroke-linejoin": "round",
-                                d: "M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z",
-                            }
-                        }
-                    }
-                    button {
-                        r#type: "button",
-                        class: styles::BOTTOM_BAR_BUTTON,
-                        disabled: **column == TaskStatus::Done,
-                        onclick: |_| {
-                            match **column {
-                                TaskStatus::ToDo => column.set(TaskStatus::InProgress),
-                                TaskStatus::InProgress => column.set(TaskStatus::Done),
-                                TaskStatus::Done => column.set(TaskStatus::Done),
-                            }
-                        },
-                        if **column != TaskStatus::Done {rsx!{
-                            svg {
-                                xmlns: "http://www.w3.org/2000/svg",
-                                fill: "none",
-                                "viewBox": "0 0 24 24",
-                                "stroke-width": "1.5",
-                                stroke: "currentColor",
-                                class: "w-6 h-6 text-gray-400 group-active:text-blue-500",
-                                path {
-                                    "stroke-lineca": "round",
-                                    "stroke-linejoin": "round",
-                                    d: "m8.25 4.5 7.5 7.5-7.5 7.5",
-                                }
-                            }
-                        }}
-                    }
-                }
-            }
-        }
-    })
-}
 
 #[component]
 fn ThreeColumnBoard(cx: Scope, board_name: BoardName) -> Element {
@@ -452,9 +256,15 @@ fn ThreeColumnBoard(cx: Scope, board_name: BoardName) -> Element {
                     class: "grow w-full h-full overflow-y-auto",
                     div {
                         class: "w-full h-full grid grid-cols-3 gap-2 overflow-y-auto",
-                        ToDoColumn {},
-                        InProgressColumn {},
-                        DoneColumn {},
+                        if model.read().dense_view {rsx!{
+                            DenseToDoColumn {}
+                            DenseInProgressColumn {}
+                            DenseDoneColumn {}
+                        }} else {rsx!{
+                            ToDoColumn {}
+                            InProgressColumn {}
+                            DoneColumn {}
+                        }}
                     },
                 }
                 FilterBar {}
@@ -662,7 +472,7 @@ fn DenseToDoColumn(cx: Scope) -> Element {
                             "viewBox": "0 0 24 24",
                             "stroke-width": "1.5",
                             stroke: "white",
-                            class: "w-6 h-6",
+                            class: "w-6 h-6 sm:w-8 sm:h-8",
                             path {
                                 "stroke-linecap": "round",
                                 "stroke-linejoin": "round",
@@ -680,7 +490,7 @@ fn DenseToDoColumn(cx: Scope) -> Element {
                         "viewBox": "0 0 24 24",
                         "stroke-width": "1.5",
                         stroke: "currentColor",
-                        class: "w-6 h-6 text-blue-500",
+                        class: "w-6 h-6 sm:w-8 sm:h-8 text-blue-500",
                         onclick: |event| {
                             event.stop_propagation();
                             model.write().dense_view = false;
@@ -852,7 +662,7 @@ fn DenseInProgressColumn(cx: Scope) -> Element {
                             "viewBox": "0 0 24 24",
                             "stroke-width": "1.5",
                             "stroke": "white",
-                            "class": "w-6 h-6",
+                            class: "w-6 h-6 sm:w-8 sm:h-8",
                             path {
                                 "stroke-linecap": "round",
                                 "stroke-linejoin": "round",
@@ -870,7 +680,7 @@ fn DenseInProgressColumn(cx: Scope) -> Element {
                         "viewBox": "0 0 24 24",
                         "stroke-width": "1.5",
                         stroke: "currentColor",
-                        class: "w-6 h-6 text-blue-500",
+                        class: "w-6 h-6 sm:w-8 sm:h-8 text-blue-500",
                         onclick: |event| {
                             event.stop_propagation();
                             model.write().dense_view = false;
@@ -1042,7 +852,7 @@ fn DenseDoneColumn(cx: Scope) -> Element {
                             "viewBox": "0 0 24 24",
                             "stroke-width": "1.5",
                             stroke: "white",
-                            class: "w-6 h-6",
+                            class: "w-6 h-6 sm:w-8 sm:h-8",
                             path {
                                 "stroke-linecap": "round",
                                 "stroke-linejoin": "round",
@@ -1060,7 +870,7 @@ fn DenseDoneColumn(cx: Scope) -> Element {
                         "viewBox": "0 0 24 24",
                         "stroke-width": "1.5",
                         stroke: "currentColor",
-                        class: "w-6 h-6 text-blue-500",
+                        class: "w-6 h-6 sm:w-8 sm:h-8 text-blue-500",
                         onclick: |event| {
                             event.stop_propagation();
                             model.write().dense_view = false;
