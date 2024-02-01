c91eb9b087d11983bdd7d8bb341874b079a8cd89
Lukas Turcani <lukasturcani93@gmail.com>
2024-02-01 13:09:36 +0000

    stuff

--- frontend/src/tags.rs
+++ frontend/src/tags.rs
@@ -81,6 +81,7 @@ pub fn Tags(cx: Scope, board_name: BoardName) -> Element {
                                     TagRow {
                                         key: "{tag.id}",
                                         tag: tag.clone(),
+                                        archived: false,
                                     }
                                 ))
                         }
@@ -122,6 +123,7 @@ pub fn Tags(cx: Scope, board_name: BoardName) -> Element {
                                     TagRow {
                                         key: "{tag.id}",
                                         tag: tag.clone(),
+                                        archived: true,
                                     }
                                 ))
                         }
@@ -160,7 +162,7 @@ pub fn Tags(cx: Scope, board_name: BoardName) -> Element {
 }
 
 #[component]
-fn TagRow(cx: Scope, tag: TagEntry) -> Element {
+fn TagRow(cx: Scope, tag: TagEntry, archived: bool) -> Element {
     let url = use_shared_state::<TagsUrl>(cx).unwrap();
     let tags = use_shared_state::<TagEntries>(cx).unwrap();
     let editing_color = use_state(cx, || false);
@@ -253,23 +255,43 @@ fn TagRow(cx: Scope, tag: TagEntry) -> Element {
                     class: "grid grid-rows-1 place-items-end",
                     div {
                         class: "flex flex-row gap-2",
-                        svg {
-                            xmlns: "http://www.w3.org/2000/svg" ,
-                            fill: "none",
-                            "viewBox": "0 0 24 24",
-                            "stroke-width": "1.5",
-                            stroke: "currentColor",
-                            class: "
-                                w-6 h-6 cursor-pointer text-gray-400
-                                sm:hover:text-blue-500 active:text-blue-500
-                            ",
-                            onclick: |_| set_tag_archived(tags.clone(), url.clone(), tag.id, true),
-                            path {
-                                "stroke-linecap": "round",
-                                "stroke-linejoin": "round",
-                                d: "m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0-3-3m3 3 3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z",
+                        if *archived {rsx! {
+                            svg {
+                                xmlns: "http://www.w3.org/2000/svg" ,
+                                fill: "none",
+                                "viewBox": "0 0 24 24",
+                                "stroke-width": "1.5",
+                                stroke: "currentColor",
+                                class: "
+                                    w-6 h-6 cursor-pointer text-gray-400
+                                    sm:hover:text-blue-500 active:text-blue-500
+                                ",
+                                onclick: |_| set_tag_archived(tags.clone(), url.clone(), tag.id, false),
+                                path {
+                                    "stroke-linecap": "round",
+                                    "stroke-linejoin": "round",
+                                    d: "m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z",
+                                }
                             }
-                        }
+                        }} else {rsx! {
+                            svg {
+                                xmlns: "http://www.w3.org/2000/svg" ,
+                                fill: "none",
+                                "viewBox": "0 0 24 24",
+                                "stroke-width": "1.5",
+                                stroke: "currentColor",
+                                class: "
+                                    w-6 h-6 cursor-pointer text-gray-400
+                                    sm:hover:text-blue-500 active:text-blue-500
+                                ",
+                                onclick: |_| set_tag_archived(tags.clone(), url.clone(), tag.id, true),
+                                path {
+                                    "stroke-linecap": "round",
+                                    "stroke-linejoin": "round",
+                                    d: "m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0-3-3m3 3 3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z",
+                                }
+                            }
+                        }}
                         svg {
                             xmlns: "http://www.w3.org/2000/svg",
                             fill: "none",
