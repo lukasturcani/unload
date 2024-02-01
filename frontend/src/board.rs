0f758c4e844341a5431341ac5cc2df243f444fe3
Lukas Turcani <lukasturcani93@gmail.com>
2024-02-01 16:29:54 +0000

    stuff

--- frontend/src/board.rs
+++ frontend/src/board.rs
@@ -111,6 +111,28 @@ fn OneColumnBoard(cx: Scope, board_name: BoardName) -> Element {
                     }
                     button {
                         r#type: "button",
+                        class: styles::BOTTOM_BAR_BUTTON,
+                        onclick: |_| {
+                            nav.push(Route::TaskArchive {
+                                board_name: board_name.clone(),
+                            });
+                        },
+                        svg {
+                            xmlns: "http://www.w3.org/2000/svg",
+                            fill: "none",
+                            "viewBox": "0 0 24 24",
+                            "stroke-width": "1.5",
+                            stroke: "currentColor",
+                            class: "w-6 h-6 text-gray-400 group-active:text-blue-500",
+                            path {
+                                "stroke-linecap": "round",
+                                "stroke-linejoin": "round",
+                                d: "m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0-3-3m3 3 3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z",
+                            }
+                        }
+                    }
+                    button {
+                        r#type: "button",
                         class: if **show_filters {
                             styles::BOTTOM_BAR_ACTIVE_BUTTON
                         } else {
@@ -181,28 +203,6 @@ fn OneColumnBoard(cx: Scope, board_name: BoardName) -> Element {
                                 "stroke-linecap": "round",
                                 "stroke-linejoin": "round",
                                 d: "M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z",
-                            }
-                        }
-                    }
-                    button {
-                        r#type: "button",
-                        class: styles::BOTTOM_BAR_BUTTON,
-                        onclick: |_| {
-                            nav.push(Route::TaskArchive {
-                                board_name: board_name.clone(),
-                            });
-                        },
-                        svg {
-                            xmlns: "http://www.w3.org/2000/svg",
-                            fill: "none",
-                            "viewBox": "0 0 24 24",
-                            "stroke-width": "1.5",
-                            stroke: "currentColor",
-                            class: "w-6 h-6 text-gray-400 group-active:text-blue-500",
-                            path {
-                                "stroke-linecap": "round",
-                                "stroke-linejoin": "round",
-                                d: "m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0-3-3m3 3 3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z",
                             }
                         }
                     }
@@ -286,6 +286,28 @@ fn BottomBar(cx: Scope, board_name: BoardName) -> Element {
                 r#type: "button",
                 class: styles::BOTTOM_BAR_BUTTON,
                 onclick: |_| {
+                    nav.push(Route::TaskArchive {
+                        board_name: board_name.clone(),
+                    });
+                },
+                svg {
+                    xmlns: "http://www.w3.org/2000/svg",
+                    fill: "none",
+                    "viewBox": "0 0 24 24",
+                    "stroke-width": "1.5",
+                    stroke: "currentColor",
+                    class: "w-6 h-6 text-gray-400 group-hover:text-blue-500",
+                    path {
+                        "stroke-linecap": "round",
+                        "stroke-linejoin": "round",
+                        d: "m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0-3-3m3 3 3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z",
+                    }
+                }
+            }
+            button {
+                r#type: "button",
+                class: styles::BOTTOM_BAR_BUTTON,
+                onclick: |_| {
                     nav.push(Route::Tags {
                         board_name: board_name.clone(),
                     });
@@ -328,28 +350,6 @@ fn BottomBar(cx: Scope, board_name: BoardName) -> Element {
                         "stroke-linecap": "round",
                         "stroke-linejoin": "round",
                         d: "M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z",
-                    }
-                }
-            }
-            button {
-                r#type: "button",
-                class: styles::BOTTOM_BAR_BUTTON,
-                onclick: |_| {
-                    nav.push(Route::TaskArchive {
-                        board_name: board_name.clone(),
-                    });
-                },
-                svg {
-                    xmlns: "http://www.w3.org/2000/svg",
-                    fill: "none",
-                    "viewBox": "0 0 24 24",
-                    "stroke-width": "1.5",
-                    stroke: "currentColor",
-                    class: "w-6 h-6 text-gray-400 group-hover:text-blue-500",
-                    path {
-                        "stroke-linecap": "round",
-                        "stroke-linejoin": "round",
-                        d: "m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0-3-3m3 3 3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z",
                     }
                 }
             }
