3932c4da4e3ee9cc39329d0350763b3fc69b6f60
Lukas Turcani <lukasturcani93@gmail.com>
2024-02-02 10:24:14 +0000

    wip

--- frontend/src/task_archive.rs
+++ frontend/src/task_archive.rs
@@ -106,21 +106,38 @@ fn Task(cx: Scope, task_id: TaskId) -> Element {
                     class: "text-white",
                     task.title.clone()
                 }
-                svg {
-                    xmlns: "http://www.w3.org/2000/svg" ,
-                    fill: "none",
-                    "viewBox": "0 0 24 24",
-                    "stroke-width": "1.5",
-                    stroke: "currentColor",
-                    class: "
-                        w-6 h-6 cursor-pointer text-gray-400
-                        sm:hover:text-blue-500 active:text-blue-500
-                    ",
-                    onclick: |_| unarchive_task(archive.clone(), url.clone(), *task_id),
-                    path {
-                        "stroke-linecap": "round",
-                        "stroke-linejoin": "round",
-                        d: "m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z",
+                div {
+                class: "flex flex-row gap-2",
+                    svg {
+                        xmlns: "http://www.w3.org/2000/svg" ,
+                        fill: "none",
+                        "viewBox": "0 0 24 24",
+                        "stroke-width": "1.5",
+                        stroke: "currentColor",
+                        class: "
+                            w-6 h-6 cursor-pointer text-gray-400
+                            sm:hover:text-blue-500 active:text-blue-500
+                        ",
+                        onclick: |_| unarchive_task(archive.clone(), url.clone(), *task_id),
+                        path {
+                            "stroke-linecap": "round",
+                            "stroke-linejoin": "round",
+                            d: "m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z",
+                        }
+                    }
+                    svg {
+                        xmlns: "http://www.w3.org/2000/svg",
+                        fill: "none",
+                        "viewBox": "0 0 24 24",
+                        "stroke-width": "1.5",
+                        stroke: "currentColor",
+                        class: "w-6 h-6 cursor-pointer text-red-600",
+                        onclick: |_| delete_task(archive.clone(), url.clone(), *task_id),
+                        path {
+                            "stroke-linecap": "round",
+                            "stroke-linejoin": "round",
+                            d: "m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0",
+                        }
                     }
                 }
             }
@@ -246,6 +263,26 @@ async fn send_unarchive_task_request(url: &Url, task_id: TaskId) -> Result<(), a
     Ok(reqwest::Client::new()
         .put(url)
         .json(&false)
+        .send()
+        .await?
+        .json::<()>()
+        .await?)
+}
+
+async fn delete_task(
+    archive: UseSharedState<TaskArchive>,
+    url: UseSharedState<TasksUrl>,
+    task_id: TaskId,
+) {
+    let url = &url.read().0;
+    let _ = send_delete_task_request(url, task_id).await;
+    get_archive(archive, url).await;
+}
+
+async fn send_delete_task_request(url: &Url, task_id: TaskId) -> Result<(), anyhow::Error> {
+    let url = url.join(&format!("tasks/{}", task_id))?;
+    Ok(reqwest::Client::new()
+        .delete(url)
         .send()
         .await?
         .json::<()>()
