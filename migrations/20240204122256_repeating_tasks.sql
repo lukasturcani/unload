eac7272acc4445b7f85ac1ae7c60d725f90d9010
Lukas Turcani <lukasturcani93@gmail.com>
2024-02-04 12:31:11 +0000

    Add repeating tasks schema

---
+++ migrations/20240204122256_repeating_tasks.sql
@@ -0,0 +1,46 @@
+CREATE TABLE
+IF NOT EXISTS repeat_daily (
+    id INTEGER PRIMARY KEY NOT NULL,
+    board_name TEXT NOT NULL,
+    task_id INTEGER NOT NULL,
+    every_n_days INTEGER NOT NULL,
+    from_date DATE NOT NULL,
+    last_done DATE,
+    FOREIGN KEY (board_name) REFERENCES boards (name),
+    FOREIGN KEY (task_id) REFERENCES tasks (id)
+);
+
+CREATE TABLE
+IF NOT EXISTS repeat_weekly (
+    id INTEGER PRIMARY KEY NOT NULL,
+    board_name TEXT NOT NULL,
+    task_id INTEGER NOT NULL,
+    day_of_week TEXT CHECK (day_of_week IN (
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    )) NOT NULL,
+    every_n_weeks INTEGER NOT NULL,
+    from_date DATE NOT NULL,
+    last_done DATE,
+    FOREIGN KEY (board_name) REFERENCES boards (name),
+    FOREIGN KEY (task_id) REFERENCES tasks (id)
+);
+
+
+CREATE TABLE
+IF NOT EXISTS repeat_monthly (
+    id INTEGER PRIMARY KEY NOT NULL,
+    board_name TEXT NOT NULL,
+    task_id INTEGER NOT NULL,
+    day_of_month INTEGER NOT NULL,
+    every_n_months INTEGER NOT NULL,
+    from_date DATE NOT NULL,
+    last_done DATE,
+    FOREIGN KEY (board_name) REFERENCES boards (name),
+    FOREIGN KEY (task_id) REFERENCES tasks (id)
+);
