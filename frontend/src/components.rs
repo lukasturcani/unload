2f92a11627fefe452223dd4c0583e2a328bd8b2e
Lukas Turcani <lukasturcani93@gmail.com>
2024-05-10 22:08:46 +0100

    Stuff

--- frontend/src/components.rs
+++ frontend/src/components.rs
@@ -1,2 +1,2 @@
+pub mod icons;
 pub mod task;
-
---
+++ frontend/src/components/icons.rs
@@ -0,0 +1,110 @@
+use dioxus::prelude::*;
+
+#[component]
+pub fn EditIcon() -> Element {
+    rsx! {
+        Icon {
+            d: "m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10",
+        }
+    }
+}
+
+#[component]
+pub fn ConfirmIcon() -> Element {
+    rsx! {
+        Icon {
+            d: "m4.5 12.75 6 6 9-13.5",
+        }
+    }
+}
+
+#[component]
+pub fn CancelIcon() -> Element {
+    rsx! {
+        Icon {
+            d: "M6 18 18 6M6 6l12 12",
+        }
+    }
+}
+
+#[component]
+pub fn PlusIcon() -> Element {
+    rsx! {
+        Icon {
+            d: "M12 4.5v15m7.5-7.5h-15",
+            stroke_width: "2",
+        }
+    }
+}
+
+#[component]
+pub fn ToDoIcon() -> Element {
+    rsx! {
+        Icon {
+            d: "M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z",
+        }
+    }
+}
+
+#[component]
+pub fn InProgressIcon() -> Element {
+    rsx! {
+        Icon {
+            d: "M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z",
+        }
+    }
+}
+
+#[component]
+pub fn DoneIcon() -> Element {
+    rsx! {
+        Icon {
+            d: "M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z",
+        }
+    }
+}
+
+#[component]
+pub fn BoltIcon() -> Element {
+    rsx! {
+        Icon {
+            d: "m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z",
+        }
+    }
+}
+
+#[component]
+pub fn CopyIcon() -> Element {
+    rsx! {
+        Icon {
+            d: "M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75",
+        }
+    }
+}
+
+#[component]
+pub fn ArchiveIcon() -> Element {
+    rsx! {
+        Icon {
+            d: "m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0-3-3m3 3 3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z",
+        }
+    }
+}
+
+#[component]
+fn Icon(d: &'static str, stroke_width: Option<&'static str>) -> Element {
+    let stroke_width = stroke_width.unwrap_or("1.5");
+    rsx! {
+        svg {
+            xmlns: "http://www.w3.org/2000/svg",
+            fill: "none",
+            "viewBox": "0 0 24 24",
+            "stroke-width": stroke_width,
+            path {
+                "stroke-linecap": "round",
+                "stroke-linejoin": "round",
+                d,
+            }
+        }
+    }
+}
