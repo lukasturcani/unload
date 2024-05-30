b3880bb573a685069a19d0fa5d5f5a8433d48251
Lukas Turcani <lukasturcani93@gmail.com>
2024-05-31 00:27:51 +0100

    wip

---
+++ frontend/src/components/input.rs
@@ -0,0 +1,66 @@
+use dioxus::prelude::*;
+
+use crate::themes::Theme;
+
+#[component]
+pub fn TextInput(id: String, label: String, value: Option<String>) -> Element {
+    let theme = use_context::<Signal<Theme>>();
+    let theme = theme.read();
+    let style = format!(
+        "
+        text-base
+        rounded-lg
+        border
+        focus:ring-blue-500 focus:border-blue-500
+        {} {}
+    ",
+        theme.border_color, theme.bg_color_2
+    );
+    let name = label.clone();
+    rsx! {
+        label {
+            class: "text-sm",
+            r#for: "{id}",
+            {label}
+        }
+        input {
+            id,
+            class: "p-2.5 {style}",
+            name,
+            required: true,
+            value,
+        }
+    }
+}
+
+#[component]
+pub fn DateInput(id: String, label: String, value: Option<String>) -> Element {
+    let theme = use_context::<Signal<Theme>>();
+    let theme = theme.read();
+    let style = format!(
+        "
+        text-base
+        rounded-lg
+        border
+        focus:ring-blue-500 focus:border-blue-500
+        {} {}
+    ",
+        theme.border_color, theme.bg_color_2
+    );
+    let name = label.clone();
+    rsx! {
+        label {
+            class: "text-sm",
+            r#for: "{id}",
+            {label}
+        }
+        input {
+            id,
+            class: "p-2.5 {style}",
+            r#type: "date",
+            name,
+            required: true,
+            value,
+        }
+    }
+}
