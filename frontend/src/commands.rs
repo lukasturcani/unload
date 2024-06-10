f3f37e0e53207c35926855aa1f3b0e2871863986
Lukas Turcani <lukasturcani93@gmail.com>
2024-06-10 11:39:24 +0100

    commands good

---
+++ frontend/src/commands.rs
@@ -0,0 +1,39 @@
+use dioxus::prelude::*;
+
+#[derive(Clone, Debug, Eq, PartialEq, Default)]
+pub struct ScrollTarget(pub Option<String>);
+
+#[component]
+pub fn ScrollCommand() -> Element {
+    let scroll_target = use_context::<Signal<ScrollTarget>>();
+    let scroll_target = &scroll_target.read().0;
+    let scroll = eval(
+        r#"
+            let elementId = await dioxus.recv();
+            document.getElementById(elementId).scrollIntoView({behavior: "smooth"});
+        "#,
+    );
+    if let Some(scroll_target) = scroll_target {
+        scroll.send(scroll_target.clone().into()).unwrap();
+    }
+    rsx! {}
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Default)]
+pub struct FocusTarget(pub Option<String>);
+
+#[component]
+pub fn FocusCommand() -> Element {
+    let focus_target = use_context::<Signal<FocusTarget>>();
+    let focus_target = &focus_target.read().0;
+    let focus = eval(
+        r#"
+            let elementId = await dioxus.recv();
+            document.getElementById(elementId).focus();
+        "#,
+    );
+    if let Some(focus_target) = focus_target {
+        focus.send(focus_target.clone().into()).unwrap();
+    }
+    rsx! {}
+}
