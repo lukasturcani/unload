6498b9e8a1e4201493897e5ab7eba0e863a7cc0f
Lukas Turcani <lukasturcani93@gmail.com>
2024-06-14 11:52:03 +0100

    nice

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,19 +1,12 @@
 use crate::commands::{FocusCommand, FocusTarget, ScrollCommand, ScrollTarget};
-use crate::model::{Board, QuickAddTasks, TagFilter, Tags, Tasks, UnloadUrl, UserFilter, Users};
+use crate::model::UnloadUrl;
 use crate::route::Route;
 use crate::themes::THEMES;
 use dioxus::prelude::*;
 
 #[component]
 pub fn App() -> Element {
-    use_context_provider(|| Signal::new(Board::default()));
     use_context_provider(|| Signal::new(UnloadUrl::default()));
-    use_context_provider(|| Signal::new(Tasks::default()));
-    use_context_provider(|| Signal::new(Users::default()));
-    use_context_provider(|| Signal::new(Tags::default()));
-    use_context_provider(|| Signal::new(QuickAddTasks::default()));
-    use_context_provider(|| Signal::new(UserFilter::default()));
-    use_context_provider(|| Signal::new(TagFilter::default()));
     use_context_provider(|| Signal::new(THEMES[0]));
     use_context_provider(|| Signal::new(ScrollTarget::default()));
     use_context_provider(|| Signal::new(FocusTarget::default()));
--- frontend/src/pages/board.rs
+++ frontend/src/pages/board.rs
@@ -1,6 +1,7 @@
 use dioxus::prelude::*;
 
 use crate::components::one_column_board::OneColumnBoard;
+use crate::model::{Board, QuickAddTasks, TagFilter, Tags, Tasks, UserFilter, Users};
 use crate::pages::three_column_board::ThreeColumnBoard;
 use crate::responsive_layout::ResponsiveLayout;
 
@@ -8,6 +9,13 @@ use shared_models::BoardName;
 
 #[component]
 pub fn Board(board_name: BoardName) -> Element {
+    use_context_provider(|| Signal::new(Board::default()));
+    use_context_provider(|| Signal::new(Tasks::default()));
+    use_context_provider(|| Signal::new(Users::default()));
+    use_context_provider(|| Signal::new(Tags::default()));
+    use_context_provider(|| Signal::new(QuickAddTasks::default()));
+    use_context_provider(|| Signal::new(UserFilter::default()));
+    use_context_provider(|| Signal::new(TagFilter::default()));
     let layout = ResponsiveLayout::from_window();
     eval(&format!(r#"document.title = "{board_name}";"#));
     rsx! {
