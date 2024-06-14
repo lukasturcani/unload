f78300200cf786f783bf5e81f38d0282b208d9cc
Lukas Turcani <lukasturcani93@gmail.com>
2024-06-14 20:42:56 +0100

    wip

--- frontend/src/pages/users.rs
+++ frontend/src/pages/users.rs
@@ -4,7 +4,7 @@ use shared_models::BoardName;
 use crate::components::nav::NavBar;
 use crate::model::UnloadUrl;
 use crate::pages::users::components::UserList;
-use crate::pages::users::model::UsersUrl;
+use crate::pages::users::model::{UserEntries, UsersUrl};
 use crate::themes::Theme;
 
 mod components;
@@ -14,15 +14,19 @@ mod model;
 pub fn Users(board_name: BoardName) -> Element {
     let url = use_context::<Signal<UnloadUrl>>();
     eval(&format!(r#"document.title = "{board_name}";"#));
-    use_context_provider(move || {
-        let url = url
-            .read()
-            .0
-            .join(&format!("/api/boards/{}/users", board_name))
-            .unwrap();
-        Signal::new(UsersUrl(url))
+    use_context_provider(|| Signal::new(UserEntries::default()));
+    use_context_provider({
+        let board_name = board_name.clone();
+        move || {
+            let url = url
+                .read()
+                .0
+                .join(&format!("/api/boards/{}/users", board_name))
+                .unwrap();
+            Signal::new(UsersUrl(url))
+        }
     });
-    rsx! { UserList {} }
+    rsx! { UsersPage { board_name } }
 }
 
 #[component]
@@ -34,6 +38,7 @@ fn UsersPage(board_name: BoardName) -> Element {
         div {
             class: "flex flex-col h-dvh w-screen {style}",
             div {
+                class: "grow",
                 UserList {}
             }
             NavBar { board_name }
