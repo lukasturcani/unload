604adef3c6d3e4a0e3f59ae717450d4ac0fcc540
Lukas Turcani <lukasturcani93@gmail.com>
2024-06-14 21:10:19 +0100

    wip

--- frontend/src/pages/users.rs
+++ frontend/src/pages/users.rs
@@ -1,5 +1,6 @@
 use dioxus::prelude::*;
-use shared_models::BoardName;
+use reqwest::Url;
+use shared_models::{BoardName, UserEntry};
 
 use crate::components::nav::NavBar;
 use crate::model::UnloadUrl;
@@ -14,8 +15,8 @@ mod model;
 pub fn Users(board_name: BoardName) -> Element {
     let url = use_context::<Signal<UnloadUrl>>();
     eval(&format!(r#"document.title = "{board_name}";"#));
-    use_context_provider(|| Signal::new(UserEntries::default()));
-    use_context_provider({
+    let users = use_context_provider(|| Signal::new(UserEntries::default()));
+    let url = use_context_provider({
         let board_name = board_name.clone();
         move || {
             let url = url
@@ -26,6 +27,7 @@ pub fn Users(board_name: BoardName) -> Element {
             Signal::new(UsersUrl(url))
         }
     });
+    use_future(move || get_users(users, url));
     rsx! { UsersPage { board_name } }
 }
 
@@ -36,12 +38,29 @@ fn UsersPage(board_name: BoardName) -> Element {
     let style = format!("{} {}", theme.text_color, theme.bg_color_1);
     rsx! {
         div {
-            class: "flex flex-col h-dvh w-screen {style}",
+            class: "flex flex-col h-dvh w-screen pt-4 {style}",
             div {
-                class: "grow",
+                class: "grow container mx-auto",
                 UserList {}
             }
             NavBar { board_name }
         }
     }
+}
+
+async fn get_users(mut users: Signal<UserEntries>, url: Signal<UsersUrl>) {
+    let url = &url.read().0;
+    if let Ok(result) = send_get_users_request(url).await {
+        users.write().0 = result;
+    }
+}
+
+async fn send_get_users_request(url: &Url) -> Result<Vec<UserEntry>, anyhow::Error> {
+    let url = url.join("users")?;
+    Ok(reqwest::Client::new()
+        .get(url.clone())
+        .send()
+        .await?
+        .json::<Vec<UserEntry>>()
+        .await?)
 }
--- frontend/src/pages/users/components/user_list.rs
+++ frontend/src/pages/users/components/user_list.rs
@@ -1,13 +1,24 @@
 use dioxus::prelude::*;
 use shared_models::UserEntry;
 
-use crate::pages::users::model::UserEntries;
+use crate::{pages::users::model::UserEntries, themes::Theme};
 
 #[component]
 pub fn UserList() -> Element {
+    let theme = use_context::<Signal<Theme>>();
+    let theme = theme.read();
+    let style = format!(
+        "
+            border rounded-lg
+            divide-y
+            {} {}
+        ",
+        theme.border_color, theme.divide_color,
+    );
     let users = use_context::<Signal<UserEntries>>();
     rsx! {
         ul {
+            class: style,
             for user in users.read().0.iter() {
                 UserListItem { user: user.clone() }
             }
@@ -19,6 +30,7 @@ pub fn UserList() -> Element {
 fn UserListItem(user: UserEntry) -> Element {
     rsx! {
         li {
+            class: "px-3 py-1",
             { user.name }
         }
     }
