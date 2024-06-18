eafe9eb7e3fc05bb9ac6da9cb53d25fe5b931348
Lukas Turcani <lukasturcani93@gmail.com>
2024-06-18 19:30:03 +0100

    wip

---
+++ frontend/src/pages/board/components/add_task_button.rs
@@ -0,0 +1,33 @@
+use dioxus::prelude::*;
+use shared_models::TaskStatus;
+
+use crate::{commands::FocusTarget, components::icons::CircledPlusIcon, themes::Theme};
+
+#[component]
+pub fn AddTaskButton(status: TaskStatus, adding_task: Signal<bool>) -> Element {
+    let mut focus_target = use_context::<Signal<FocusTarget>>();
+    let theme = use_context::<Signal<Theme>>();
+    let theme = theme.read();
+    let style = format!("border-t {}", theme.border_color);
+    rsx! {
+        button {
+            class: "
+                h-12 shrink-0 grow-0 flex flex-row justify-center items-center
+                {style}
+            ",
+            onclick: move |_| async move {
+                if adding_task() {
+                    focus_target.set(
+                        FocusTarget(Some(format!("new-{status:#?}-task-title-input")))
+                    );
+                } else {
+                    adding_task.set(true);
+                }
+            },
+            div {
+                class: "size-6",
+                CircledPlusIcon {}
+            }
+        }
+    }
+}
