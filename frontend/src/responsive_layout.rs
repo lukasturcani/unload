4df3c35ae34e2ab2f3779baa495c145b299c9a40
Lukas Turcani <lukasturcani93@gmail.com>
2024-06-22 21:16:46 +0100

    wip

--- frontend/src/responsive_layout.rs
+++ frontend/src/responsive_layout.rs
@@ -6,16 +6,25 @@ pub enum ResponsiveLayout {
 
 impl ResponsiveLayout {
     pub fn from_window() -> Self {
-        let width = web_sys::window()
-            .unwrap()
-            .inner_width()
-            .unwrap()
-            .as_f64()
-            .unwrap();
-        if width < 640.0 {
+        if width() < 640.0 {
             ResponsiveLayout::Narrow
         } else {
             ResponsiveLayout::Wide
         }
     }
+}
+
+#[cfg(target_arch = "wasm32")]
+fn width() -> f64 {
+    web_sys::window()
+        .unwrap()
+        .inner_width()
+        .unwrap()
+        .as_f64()
+        .unwrap()
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn width() -> f64 {
+    dioxus::mobile::window().inner_size().width as f64
 }
