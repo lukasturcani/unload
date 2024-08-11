e81f416de6ac3f97acb3aa13ac641fd20ee1bcc4
Lukas Turcani <lukasturcani93@gmail.com>
2024-08-11 14:35:29 +0100

    wip

--- frontend/src/components/input.rs
+++ frontend/src/components/input.rs
@@ -1,6 +1,6 @@
 use dioxus::prelude::*;
 
-use crate::{commands::ScrollTarget, themes::Theme};
+use crate::themes::Theme;
 
 #[component]
 pub fn TextInput(id: String, label: String, value: Option<String>) -> Element {
@@ -15,7 +15,6 @@ pub fn TextInput(id: String, label: String, value: Option<String>) -> Element {
     ",
         theme.border_color, theme.bg_color_2, theme.focus_color
     );
-    let mut scroll = use_context::<Signal<ScrollTarget>>();
     let name = label.clone();
     rsx! {
         label {
@@ -29,12 +28,8 @@ pub fn TextInput(id: String, label: String, value: Option<String>) -> Element {
             name,
             required: true,
             value,
-            onmounted: move |event| {
-                let id = id.clone();
-                async move {
-                    let _ = event.set_focus(true).await;
-                    scroll.set(ScrollTarget(Some(id.clone())));
-                }
+            onmounted: move |event|  async move {
+                let _ = event.set_focus(true).await;
             },
         }
     }
