f360f1ecd2ec4e4fc4eb59385a1be6fa99507fae
Lukas Turcani <lukasturcani93@gmail.com>
2024-08-15 15:52:44 +0100

    wip

--- frontend/src/pages/board/components/tag_icon.rs
+++ frontend/src/pages/board/components/tag_icon.rs
@@ -5,39 +5,15 @@ use crate::{components::icons::CancelIcon, pages::board::model::TagFilter, theme
 
 #[component]
 pub fn FilterBarTagIcon(tag_id: TagId, tag_data: TagData) -> Element {
-    let theme = use_context::<Signal<Theme>>();
-    let theme = theme.read();
-    let color = match tag_data.color {
-        Color::Black => theme.color1_button,
-        Color::White => theme.color2_button,
-        Color::Gray => theme.color3_button,
-        Color::Silver => theme.color4_button,
-        Color::Maroon => theme.color5_button,
-        Color::Red => theme.color6_button,
-        Color::Purple => theme.color7_button,
-        Color::Fushsia => theme.color8_button,
-        Color::Green => theme.color9_button,
-        Color::Lime => theme.color10_button,
-        Color::Olive => theme.color11_button,
-        Color::Yellow => theme.color12_button,
-        Color::Navy => theme.color13_button,
-        Color::Blue => theme.color14_button,
-        Color::Teal => theme.color15_button,
-        Color::Aqua => theme.color16_button,
-    };
-    let style = "rounded border-2";
     rsx! {
-        div {
-            class: "
-                group
-                flex flex-row items-center
-                px-1.5 py-0.5
-                {style} {color}
-            ",
-            FilterButton {
-                aria_label: "toggle {tag_data.name} filter",
-                content: "# {tag_data.name}",
-                tag_id,
+        TagIcon {
+            color: tag_data.color,
+            body: rsx!{
+                FilterButton {
+                    aria_label: "toggle {tag_data.name} filter",
+                    content: "# {tag_data.name}",
+                    tag_id,
+                }
             }
         }
     }
@@ -66,7 +42,7 @@ fn FilterButton(aria_label: String, content: String, tag_id: TagId) -> Element {
 }
 
 #[component]
-pub fn TagIcon(color: Color, on_unassign_tag: EventHandler<TagId>, body: Element) -> Element {
+pub fn TagIcon(color: Color, body: Element) -> Element {
     let theme = use_context::<Signal<Theme>>();
     let theme = theme.read();
     let color = match color {
@@ -127,7 +103,6 @@ pub fn FilteringTaskTagIcon(
     rsx! {
         TagIcon {
             color: tag_data.color,
-            on_unassign_tag,
             body: rsx! {
                 FilterButton {
                     aria_label: "toggle {tag_data.name} filter",
