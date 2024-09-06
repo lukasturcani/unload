d4c1b6bb0fbb9d089b03df86d0cb434f3f542873
Lukas Turcani <lukasturcani93@gmail.com>
2024-09-06 12:28:34 +0100

    Add better error reproting

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -27,6 +27,7 @@ use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::try_join;
 use tracing::debug_span;
+use tracing::error;
 use tracing::Instrument;
 
 struct TaskRow {
@@ -85,6 +86,7 @@ pub struct AppError(anyhow::Error);
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
+        error!("{}", self.0);
         (
             StatusCode::INTERNAL_SERVER_ERROR,
             format!("Internal server error: {}", self.0),
