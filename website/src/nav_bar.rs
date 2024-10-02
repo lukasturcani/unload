ea45caf0a13d352bb875fd0d0ebde15ac6bd5e33
Lukas Turcani <lukasturcani93@gmail.com>
2024-10-02 12:21:44 +0100

    good

--- website/src/nav_bar.rs
+++ website/src/nav_bar.rs
@@ -7,12 +7,10 @@ pub fn NavBar() -> Element {
         nav {
             class: "bg-background fixed w-full z-20 top-0 start-0 border-b border-gray-600",
             div {
-                class: "max-w-screen-xl flex flex-wrap items-center \
-                    justify-between mx-auto p-4",
+                class: "max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4",
                 Logo {}
                 div {
-                    class: "flex md:order-2 space-x-3 md:space-x-0 \
-                        rtl:space-x-reverse",
+                    class: "flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse",
                     AppLink {}
                     ToggleSectionListButton {}
                 }
