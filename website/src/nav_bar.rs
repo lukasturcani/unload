84817f943f4a6d71409f91013d8588ff826f928d
Lukas Turcani <lukasturcani93@gmail.com>
2024-10-02 13:45:33 +0100

    wip

--- website/src/nav_bar.rs
+++ website/src/nav_bar.rs
@@ -1,4 +1,5 @@
 use dioxus::prelude::*;
+use dioxus_html_macro::html;
 use dioxus_sdk::{i18n::use_i18, translate};
 
 #[component]
@@ -24,7 +25,25 @@ pub fn NavBar() -> Element {
 
 #[component]
 fn LanguageSelectionButton() -> Element {
-    rsx! {}
+    html! {
+              <button id="dropdownNavbarLink" "data-dropdown-toggle"="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
+                    {"Dropdown"} <svg class="w-2.5 h-2.5 ms-2.5" "aria-hidden"="true" xmlns="http://www.w3.org/2000/svg" fill="none" view_box="0 0 10 6">
+      <path stroke="currentColor" stroke_linecap="round" stroke_linejoin="round" stroke_width="2" d="m1 1 4 4 4-4"/>
+    </svg></button>
+              <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                  <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria_labelledby="dropdownLargeButton">
+                    <li>
+                      <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{"Dashboard"}</a>
+                    </li>
+                    <li>
+                      <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{"Settings"}</a>
+                    </li>
+                    <li>
+                      <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{"Earnings"}</a>
+                    </li>
+                  </ul>
+              </div>
+      }
 }
 
 #[component]
