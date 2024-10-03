079e213a3bee21b092ccb62a9be20fdb8d5a7bd0
Lukas Turcani <lukasturcani93@gmail.com>
2024-10-03 13:39:25 +0100

    wip

--- website/src/cards.rs
+++ website/src/cards.rs
@@ -4,7 +4,7 @@ use dioxus::prelude::*;
 pub fn Card(children: Element) -> Element {
     rsx! {
         div {
-            class: "max-w-4xl p-6 border rounded-lg shadow bg-background-card border-gray-700",
+            class: "p-6 border rounded-lg shadow bg-background-card border-gray-700",
             {children}
         }
     }
