b071431e51a225a5b2bd3699a194bdc72df5328f
Lukas Turcani <lukasturcani93@gmail.com>
2024-10-04 22:47:26 +0100

    wip

--- tools/src/bin/translate.rs
+++ tools/src/bin/translate.rs
@@ -1,10 +1,11 @@
+use clap::Parser;
 use frontend::translations;
 use openai_api_rs::v1::{
     api::OpenAIClient,
-    chat_completion::{self, MessageRole},
+    chat_completion::{self, ChatCompletionMessage, ChatCompletionRequest, Content, MessageRole},
     common::GPT4_O,
-    completion::CompletionRequest,
 };
+use shared_models::SupportedLanguage;
 
 #[derive(Parser)]
 struct Cli {
@@ -17,20 +18,22 @@ async fn main() {
     let client = OpenAIClient::new(cli.openai_api_key);
     let english = translations::Translation::from(SupportedLanguage::English)
         .to_json()
-        .as_str();
-    println!("{english}");
-    return;
-    let completion_request = CompletionRequest::new(
+        .to_string();
+    let completion_request = ChatCompletionRequest::new(
         GPT4_O.to_string(),
         vec![
             ChatCompletionMessage {
                 role: MessageRole::system,
                 content: Content::Text(
-                    format!("Translate the following text into Slovak. \
+                    format!("Translate the following text into sk. \
                     It is for a to-do list app. It consists of tasks which are placed in 3 columns, to do \
-                    in progress and done.\n```json\n{english}\n```"),
+                    in progress and done. \
+                    \n```json\n{english}\n```"),
                 ),
-            }
+                name: None,
+                tool_calls: None,
+                tool_call_id: None,
+            },
         ],
     );
     let choices = client
