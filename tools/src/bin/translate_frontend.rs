431261134ab535e71245ade8cf05f2403c84945a
Lukas Turcani <lukasturcani93@gmail.com>
2024-10-05 17:26:42 +0100

    wip

--- tools/src/bin/translate_frontend.rs
+++ tools/src/bin/translate_frontend.rs
@@ -51,7 +51,7 @@ async fn main() {
         .map(|(l, s)| BatchFileLine {
             custom_id: format!("frontend-{}-request", l.name()),
             method: "POST".to_string(),
-            url: format!("/v1/chat/completions"),
+            url: "/v1/chat/completions",
             body: ChatCompletionRequest::new(
                 GPT4_O.to_string(),
                 vec![ChatCompletionMessage {
